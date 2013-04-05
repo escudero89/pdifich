1c19bda6acb8ecc6f98073e38da20a8b59b80753
Cristian Escudero <escudero89@gmail.com>
2013-04-04 21:30:44 -0300

    Added eje1

--- Guia 1/Cristian/guia3.cpp
+++ Guia 1/Cristian/guia3.cpp
@@ -25,7 +25,17 @@ void guia3_eje1(const char *filename) {
 
 }
 
-/// Segundo Ejercicio
+/// Segundo Ejercicio (suposicion debajo)
+// histo1.tif : Distribucion bastante pareja, mas densidad en los tonos oscuros. Probablemente sea una imagen con un buen contraste, con detalles oscuros.
+    // => Suposicion: ImagenC; Real: No son tan parecidos los histogramas.
+// histo2.tif : Distribucion concentrada alrededor del gris100. Mal contraste. Imagen oscura. Poco variedad de intensidad.
+    // => Suposicion: ImagenA; Real: Correcto.
+// histo3.tif : Aun mas concentrada que la anterior, alrededor de gris25. Mal contraste. Imagen muy oscura.
+    // => Suposicion: ImagenE; Real: Correcto.
+// histo4.tif : Caso inverso al anterior, con la concentracion en gris225. Imagen brillosa, con algunos detalles de ~gris80. 
+    // => Suposicion: ImagenB; Real: Correcto.
+// histo5.tif : Distribucion bastante distribuida. Contraste medio. Mayormente brillante.
+    // => Suposicion: ImagenD; Real: Correcto.
 
 int main (int argc, char* argv[]) {
 
