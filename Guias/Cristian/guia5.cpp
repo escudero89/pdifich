8961ca3c5d04dc9f63a3b2501961608101b81c45
Cristian Escudero <escudero89@gmail.com>
2013-05-01 00:13:25 -0300

    Adentro 4.1, 4.2, me fui a hacer el 3, para luego hacer el 4.3, y el 4.4 ... para luego ir al 6... fuck ajajja

--- Guias/Cristian/guia5.cpp
+++ Guias/Cristian/guia5.cpp
@@ -159,9 +159,8 @@ CImgList<> get_magnitude_phase(CImgList<> fourier) {
     return retorno;
 }
 
-/// A partir de la imagen de magnitud y fase, las une y retorna la imagen procesada antitransformada
-CImg<double> get_image_from_magn_phse(CImg<double> magnitud, CImg<double> fase) {
-
+/// A partir de la magnitud y fase, obtiene la imagen en componentes complejas
+CImgList<> get_fft_from_magn_phse(CImg<double> magnitud, CImg<double> fase) {
     CImgList<> real_imag(2, magnitud.width(), magnitud.height());
 
     complex<double> j(0, 1);
@@ -173,7 +172,22 @@ CImg<double> get_image_from_magn_phse(CImg<double> magnitud, CImg<double> fase) 
         real_imag[1](x, y) = std::imag(resultado);
     }
 
-    return real_imag.get_FFT(true)[0];
+    return real_imag;
+}
+
+/// Version sobrecargado de lo anterior
+CImgList<>  get_fft_from_magn_phse(CImgList<> magnitud_fase) {
+    return get_fft_from_magn_phse(magnitud_fase[0], magnitud_fase[1]);
+}
+
+/// A partir de la imagen de magnitud y fase, las une y retorna la imagen procesada antitransformada
+CImg<double> get_image_from_magn_phse(CImg<double> magnitud, CImg<double> fase) {
+    return get_fft_from_magn_phse(magnitud, fase).get_FFT(true)[0];
+}
+
+/// Version sobrecargado de lo anterior
+CImg<double> get_image_from_magn_phse(CImgList<> magnitud_fase) {
+    return get_image_from_magn_phse(magnitud_fase[0], magnitud_fase[1]);
 }
 
 /// EJERCICIO 2 INCISO 1
@@ -231,14 +245,145 @@ void guia5_eje2_part2(const char * filename, const char * oth_file) {
     (img1, img2, imgs_mixed).display("Original 1, 2, y mezcla [mg1/ph2] y [mg2/ph1]", 0);
 }
 
+/// EJERCICIO 3
+/* Pasos segun fer:
+1- definis mascara 21x21
+2.0- definis m = filtro.width (21)
+2.00- definis n = filtro.height (21)
+2- definis P = img.width + m - 1
+3- definis Q = img.height + n - 1
+4- imagen.resize(p,q,-100,-100,0)
+5- filtro.resize(p,q,-100,-100,0)
+6- fft_i = imagen.fft
+7- fft_f = filtro.fft
+8- resultado = fft_i * fft_j
+9- salida = resultado.ifft
+10- salida_fin = resultado.crop(m/2, n/2, p - m/2, q - n/2);
+11- tomas mate
+
+por que haces todo eso?
+porque se te agrandan las imagenes
+como en señales, tenes que hacer zero padding
+el zero padding aca significa agrandar la imagen y 
+agrandar el filtro a la suma de sus tamaños, como en señales
+
+asi te queda despues del zero padding y ANTES de transformar:
+1 1 1 0 0 0 0 
+1 1 1 0 0 0 0 
+1 1 1 0 0 0 0
+0 0 0 0 0 0 0 
+0 0 0 0 0 0 0
+*/
+
+/// EJERCICIO 4
+/// Obtengo la distancia al punto (x, y) desde cada puntos de una matriz
+CImg<double> get_D_matriz(CImg<double> base, int x = -1, int y = -1) {
+
+    CImg<double> D_matriz(base.width(), base.height());
+
+    // Si no especificamos el punto (-1, -1), toma el centro de la matriz
+    if (x == -1 && y == -1) {
+        x = D_matriz.width() / 2;
+        y = D_matriz.height() / 2;
+    } 
+
+    cimg_forXY(D_matriz, u, v) {
+        D_matriz(u, v) = pow(pow(u - x, 2) + pow(v - y, 2), 0.5);
+    }
+
+    return D_matriz;
+}
+
+/// Obtengo el filtro Butterworth (orden 2 es una buena eleccion entre pasabajo y ringing)
+CImgList<> get_butterworth(CImg<double> base, unsigned int cutoff_frecuency, unsigned int orden = 2) {
+
+    CImg<double> butterworth(base.width(), base.height()),
+        D_matriz(get_D_matriz(base));
+
+    // Aplico formula de Butterworth (p173)
+    cimg_forXY(butterworth, u, v) {
+        double factor = pow(D_matriz(u,v) / cutoff_frecuency, 2 * orden);
+        butterworth(u, v) = 1 / (1 + factor);
+    }
+
+    // Y desshifteo porque asi es la vida
+    butterworth.shift(-butterworth.width()/2, -butterworth.height()/2, 0, 0, 2);
+
+    // La parte imaginaria siempre es 0
+    return (butterworth, butterworth.get_fill(0));
+}
+
+
+/// Obtengo un filtro (ideal), parte real y parte imaginaria (que es siempre 0 para que sea zero-phase-shift)
+CImgList<> get_filter(CImg<double> base, unsigned int cutoff_frecuency) {
+
+    // La base solo la uso para determinar el tamanho
+    CImg<double> filtro(base.width(), base.height()),
+        D_matriz(get_D_matriz(base));
+
+    // Aplico formula para construir filtro ideal (p167)
+    cimg_forXY(filtro, u, v) {
+        filtro(u, v) = (D_matriz(u, v) <= cutoff_frecuency) ? 1 : 0;
+    }
+
+    filtro.shift(-filtro.width()/2, -filtro.height()/2, 0, 0, 2);
+
+    return (filtro, filtro.get_fill(0));
+}
+
+/// Retorno una convolucion de una imagen con un filtro trabajando en el espectro de frec
+CImg<double> get_img_from_filter(CImg<double> base, CImgList<> magnitud_filtro) {
+
+    // Obtengo la transformada y su magnitud/fase
+    CImgList<> fft = base.get_FFT();
+
+    // Multiplico por el filtro
+    cimg_forXY(fft[0], u, v) {
+        complex<double> 
+            part1(fft[0](u, v), fft[1](u, v)),
+            part2(magnitud_filtro[0](u, v), magnitud_filtro[1](u, v)),
+            resultado = part1 * part2;
+        
+        fft[0](u, v) = real(resultado);
+        fft[1](u, v) = imag(resultado);
+    }
+
+    CImg<double> prueba = fft.get_FFT(true)[0];
+
+    (
+        base,
+        magnitud_filtro[0], 
+        get_magnitude_phase(base.get_FFT())[0].get_log(), 
+        get_magnitude_phase(prueba.get_FFT())[0].get_log(), 
+        get_magnitude_phase(fft)[1], 
+        prueba
+    ).display("Base, PB, Base con PB", 0);
+}
+
+void guia5_eje4_part1(
+    const char * filename, 
+    const unsigned int option_extra = 10.0,
+    const unsigned int option_extra_2 = 2) {
+
+    CImg<double> base(filename),
+        filtro_ideal(base.width(), base.height());
+
+    get_img_from_filter(base, get_filter(base, option_extra));
+
+    get_img_from_filter(base, get_butterworth(base, option_extra, option_extra_2));
+
+}
+
+
 int main (int argc, char* argv[]) {
 
     const char* filename = cimg_option("-i", "../../img/avioncito.png", "Image");
     const char* oth_file = cimg_option("-a", "../../img/huang1.jpg", "Another Image");
     
-    //const unsigned char op1_level_gray = cimg_option("-g", 33, "Max Level Gray Water");
+    const unsigned int option_extra = cimg_option("-o", 33, "Option Extra");
+    const unsigned int option_extra_2 = cimg_option("-n", 2, "Option Extra_2");
 
-    guia5_eje2_part2(filename, oth_file);
+    guia5_eje4_part1(filename, option_extra, option_extra_2);
 
     return 0;
 }
