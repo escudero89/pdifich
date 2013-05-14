85d3118d31b5feb9667a06eebaeacc2c4aa1cb8f
marcosy <marcosyedro1@hotmail.com>
2013-05-14 18:31:13 -0300

    Ya casi tamo vieja

--- Guias/TPFINAL/main.cpp
+++ Guias/TPFINAL/main.cpp
@@ -3,16 +3,55 @@
 #include <iostream>
 #include "../../CImg-1.5.4/CImg.h"
 #include "../../PDI_functions.h"
-#include "../Cristian/funciones.h"
 #include <cstdlib>
 #include <complex>
 #include <string>
 #include <cmath>
-
+#include <cassert>
 
 using namespace cimg_library;
 using namespace std;
 
+/// Retorno una convolucion de una imagen con un filtro trabajando en el espectro de frec
+CImg<double> get_img_from_filter(CImg<double> base, CImgList<double> filtro) {
+
+    // Obtengo la transformada y su magnitud/fase
+    CImgList<double> fft = base.get_FFT();
+
+    // Multiplico por el filtro
+    cimg_forXY(fft[0], u, v) {
+        complex<double>
+            part1(fft[0](u, v), fft[1](u, v)),
+            part2(filtro[0](u, v), filtro[1](u, v)),
+            resultado = part1 * part2;
+
+        fft[0](u, v) = real(resultado);
+        fft[1](u, v) = imag(resultado);
+    }
+
+    CImg<double> resultado = fft.get_FFT(true)[0];
+
+    return resultado;
+}
+
+
+/// Obtengo la distancia al punto (x, y) desde cada puntos de una matriz
+CImg<double> get_D_matriz(CImg<double> base, int x = -1, int y = -1) {
+
+    CImg<double> D_matriz(base.width(), base.height());
+
+    // Si no especificamos el punto (-1, -1), toma el centro de la matriz
+    if (x == -1 && y == -1) {
+        x = D_matriz.width() / 2;
+        y = D_matriz.height() / 2;
+    }
+
+    cimg_forXY(D_matriz, u, v) {
+        D_matriz(u, v) = pow(pow(u - x, 2) + pow(v - y, 2), 0.5);
+    }
+
+    return D_matriz;
+}
 
 /// Genera el filtro H(u, v) para filtrado homomorfico
 CImgList<> get_H_homomorphic(
@@ -23,7 +62,7 @@ CImgList<> get_H_homomorphic(
     double c = 1.0) {
 
     CImg<double> H(base.width(), base.height()),
-        D_matriz(cimg_ce::get_D_matriz(base));
+        D_matriz(get_D_matriz(base));
 
     cimg_forXY(H, u, v) {
         double factor = 1 - exp(-c * pow(D_matriz(u, v), 2) / pow(cutoff_frecuency, 2));
@@ -44,13 +83,8 @@ CImg<double> get_image_homomorphic(CImg<double> base, CImgList<> H) {
     // Una imagen f(x, y) = i(x, y) * r(x, y), por lo que aplicamos log para separarlos
     CImg<double>
         base_normalizada(base.get_normalize(1, 100)),
-        logarithm(cimg_ce::get_log(base_normalizada)),
+        logarithm(base_normalizada.get_log()),
         filtrado;
-
-    (base_normalizada, base_normalizada.get_equalize(256)).display("Imagen Original, Imagen Ecualizada");
-    CImgDisplay hist_wdw;
-    base_normalizada.get_normalize(0, 255).get_histogram(256, 0, 255).display_graph(hist_wdw, 3);
-    base_normalizada.get_normalize(0, 255).get_equalize(256).get_histogram(256, 0, 255).display_graph(hist_wdw, 3);
 
     // Ahora los tengo separado teoricamente, aplico fourier (esta dentro de la funcion)
     // Y aplico el filtro S(u, v) = H(u, v) * F_i(u,v) + H(u,v) * F_r(u,v)
@@ -81,32 +115,124 @@ CImg<double> correccionPsi(CImg<double> img, double psi = 3){
 CImgList<double> decouplingLR(
     CImg<double> img, int option_fc, double option_gl, double option_gh, double option_c){
 
-    CImgList<double> f_luminancia(get_H_homomorphic(base, option_fc, option_gl, option_gh, option_c));
-    CImgList<double> f_reflactancia(get_H_homomorphic(base, option_fc, option_gh, option_gl, option_c));
+    CImgList<double> f_luminancia(get_H_homomorphic(img, option_fc, option_gl, option_gh, option_c));
+    CImgList<double> f_reflactancia(get_H_homomorphic(img, option_fc, option_gh, option_gl, option_c));
 
-    CImg<double> luminancia(get_image_homomorphic(base, f_luminancia).get_normalize(0, 255));
-    CImg<double> reflactancia(get_image_homomorphic(base, f_reflactancia).get_normalize(0, 255));
+    CImg<double> luminancia(get_image_homomorphic(img, f_luminancia).get_normalize(0, 255));
+    CImg<double> reflactancia(get_image_homomorphic(img, f_reflactancia).get_normalize(0, 255));
 
     return (luminancia, reflactancia);
 
 }
 
+///Dadas dos imagenes de intensidad una de dia y una de noche, las desacopla en Luminancia y Reflactancia
+/// y luego obtiene el cociente Lday/Lnight
+CImg<double> ratioDayNightBG(CImg<double> daytime, CImg<double> nighttime,
+                             int option_fc, double option_gl, double option_gh, double option_c){
+
+    CImgList<double> LRday(decouplingLR(daytime, option_fc, option_gl, option_gh, option_c));
+    CImgList<double> LRnight(decouplingLR(nighttime, option_fc, option_gl, option_gh, option_c));
+    CImg<double> ratio(daytime.width(), daytime.height());
+
+    cimg_forXY(daytime, x, y){
+        ratio(x,y) = LRday[0](x,y) / LRnight[0](x,y);
+    }
+
+    return ratio;
+}
+
+///Recibimos la imagen de intensidad a mejorar y el ratio calculado anteriormente
+/// Devolvemos la imagen mejorada
+CImg<double> denighting(CImg<double> base, CImg<double> ratio,
+                        int option_fc, double option_gl, double option_gh, double option_c){
+
+    CImgList<double> LRbase(decouplingLR(base, option_fc, option_gl, option_gh, option_c));
+
+    cimg_forXY(LRbase[0],x,y){
+        //En este paso hacemos el producto entre la reflactancia de la imagen a mejorar
+        //y la luminancia obtenida al multiplicar el ratio por la luminancia de la base
+        //f(x,y) = Ldenight(x,y) * R(x,y)
+        base(x,y) = (LRbase[0](x,y) * ratio(x,y)) * LRbase[1](x,y);
+
+    }
+
+    return base;
+}
+
+///Funcion principal que hace todo
+void nighttimeEnhacement(   const char* day_file,
+                            const char* night_file,
+                            const char* images_file,
+                            int option_fc, double option_gl,
+                            double option_gh, double option_c) {
+
+    CImg<double> daytime_bg(day_file);
+    CImg<double> nighttime_bg(night_file);
+
+    daytime_bg.RGBtoHSI();
+    nighttime_bg.RGBtoHSI();
+
+    CImg<double> ratio(ratioDayNightBG(daytime_bg.get_channel(2),
+                                       nighttime_bg.get_channel(2),
+                                       option_fc, option_gl, option_gh, option_c));
+
+    /// Abrimos y trabajamos imagen por imagen
+    ifstream f(images_file);
+    string image_file;
+
+    // Si no pudo abrirlo, problema vieja
+    assert(f.is_open());
+
+    unsigned int contador = 1;
+
+    // Vamos recorriendo cada path en el archivo y analizando la imagen
+    while(!f.eof()) {
+
+        f >> image_file;
+
+        CImg<double> image(image_file.c_str());
+        CImg<double> procesado(denighting(image, ratio, option_fc, option_gl, option_gh, option_c ));
+
+        procesado.get_shared_channel(2).normalize(0, 1);
+        procesado.HSItoRGB();
+        procesado.save("img_out/resultado.png", contador);
+
+        contador++;
+    }
+
+    f.close();
+
+}
+
 int main(int argc, char *argv[]){
-    const char* filename = cimg_option("-i","img/prueba.jpg", "Imagen de entrada");
+    const char* day_filename = cimg_option("-dayf","img_in/prueba.jpg", "Imagen de entrada");
+    const char* night_filename = cimg_option("-nightf","img_in/prueba.jpg", "Imagen de entrada");
+    const char* images_filename = cimg_option("-imagesf","images.txt", "Imagen de entrada");
     const double psi = cimg_option("-psi", 3.0, "Factor de correccion psi");
 
     const int fc = cimg_option("-fc", 150, "Frecuencia de Corte");
     const double gl = cimg_option("-gl", 1.0, "Gamma Low");
     const double gh = cimg_option("-gh", 0.0, "Gamma High");
-    const double c = cimg_option("-c", 0.0, "Offset");
+    const double c = cimg_option("-c", 1.0, "Offset");
 
-    CImg<double> base(filename);
-    base.RGBtoHSI();
+    nighttimeEnhacement(day_filename, night_filename, images_filename, fc, gl, gh, c);
 
+ /*
     CImg<double> corregida(correccionPsi(base, psi));
+    CImg<double> couplingImage(corregida);
+    CImgList<double> LR(decouplingLR(corregida.get_channel(2), fc, gl, gh, c));
 
+    cimg_forXY(LR[0], x, y){
+        couplingImage(x,y,2) = LR[0](x,y) * LR[1](x,y);
+    }
+    couplingImage.get_shared_channel(2).normalize(0, 1);
 
-    (base, corregida, decouplingLR(corregida.get_channel(2))).display();
+    (couplingImage.get_channel(2),corregida.get_channel(2))
+        .display();
 
+    (base.get_HSItoRGB(),
+     corregida.get_HSItoRGB(),
+     LR, couplingImage.get_HSItoRGB()).display();
+*/
     return 0;
 }
