d3da2f14400034b8e20bf94a4fdd83fd1554ef3c
Cristian Escudero <escudero89@gmail.com>
2013-05-29 02:13:59 -0300

    fusion videos

---
+++ Guias/TPFINAL/fusion_videos.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+% Para llamar, haces: %
+% sh fusion_videos video1.ext video2.ext output.extension %
+
+ffmpeg -i $1 -vf "[in] pad=2*iw:ih [left]; movie=$2 [right]; [left][right] overlay=main_w/2:0 [out]" -b:v 3000k $3
+
