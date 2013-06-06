ba7a9ef8d166880dbcd59ed7d671a46fda1d404a
Cristian Escudero <escudero89@gmail.com>
2013-06-05 22:33:02 -0300

    Preparandome para manhana

--- Guias/Cristian/template.cpp
+++ Guias/Cristian/template.cpp
@@ -15,15 +15,30 @@ using namespace cimg_library;
 
 //g++ -o template template.cpp -O0 -lm -lpthread -lX11 -lfftw3
 
-
 /// SUBLIME TEXT
+void funcion(
+	const char * filename,
+	const unsigned int A,
+	const int B,
+	const double C) {
+
+
+}
 
 int main (int argc, char* argv[]) {
 
     srand(time(0));
 
     const char* _filename = cimg_option("-i", "training/???", "Imagen");
-   
+    
+    const unsigned int _a = cimg_option("-a", "training/???", "Parametro a");
+    
+    const int _b = cimg_option("-b", "training/???", "Parametro b");
+    
+    const double _c = cimg_option("-c", "training/???", "Parametro c");
+
+
+    funcion(_filename, _a, _b, _c);
 
     return 0;
 }
